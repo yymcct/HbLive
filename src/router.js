546c049ae5275568d889618f4d19a21175c5aa13
yymcct <yymcct@hotmail.com>
2019-12-02 11:08:17 +0800

    添加路由,  添加页面

---
+++ src/router.js
@@ -0,0 +1,44 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+const routes = [
+  {
+    path: '*',
+    redirect: '/live'
+  },
+  {
+    name: 'goods',
+    component: () => import('./view/goods'),
+    meta: {
+      title: '商品详情'
+    }
+  },
+  {
+    name: 'live',
+    component: () => import('./view/live'),
+    meta: {
+      title: '直播'
+    }
+  }
+];
+
+// add route path
+routes.forEach(route => {
+  route.path = route.path || '/' + (route.name || '');
+});
+
+const router = new Router({ routes });
+
+router.beforeEach((to, from, next) => {
+  const title = to.meta && to.meta.title;
+  if (title) {
+    document.title = title;
+  }
+  next();
+});
+
+export {
+  router
+};
---
+++ src/view/live/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="goods">
+    Hello
+  </div>
+</template>
+
+<script>
+import {
+  Tag,
+  Col,
+  Icon,
+  Cell,
+  CellGroup,
+  Swipe,
+  Toast,
+  SwipeItem,
+  GoodsAction,
+  GoodsActionIcon,
+  GoodsActionButton
+} from 'vant';
+
+export default {
+  components: {
+    [Tag.name]: Tag,
+    [Col.name]: Col,
+    [Icon.name]: Icon,
+    [Cell.name]: Cell,
+    [CellGroup.name]: CellGroup,
+    [Swipe.name]: Swipe,
+    [SwipeItem.name]: SwipeItem,
+    [GoodsAction.name]: GoodsAction,
+    [GoodsActionIcon.name]: GoodsActionIcon,
+    [GoodsActionButton.name]: GoodsActionButton
+  },
+
+  data() {
+    return {
+      goods: {
+        title: '美国伽力果（约680g/3个）',
+        price: 2680,
+        express: '免运费',
+        remain: 19,
+        thumb: [
+          'https://img.yzcdn.cn/public_files/2017/10/24/e5a5a02309a41f9f5def56684808d9ae.jpeg',
+          'https://img.yzcdn.cn/public_files/2017/10/24/1791ba14088f9c2be8c610d0a6cc0f93.jpeg'
+        ]
+      }
+    };
+  },
+
+  methods: {
+    formatPrice() {
+      return '¥' + (this.goods.price / 100).toFixed(2);
+    },
+
+    onClickCart() {
+      this.$router.push('cart');
+    },
+
+    sorry() {
+      Toast('暂无后续逻辑~');
+    }
+  }
+};
+</script>
+
+<style lang="less">
+
+</style>
