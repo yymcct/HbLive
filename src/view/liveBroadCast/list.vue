ab9da3327696f09b6e0d8669aa87e1d98e0510ba
yymcct <yymcct@hotmail.com>
2019-12-11 14:04:21 +0800

    添加scroll 组件

--- src/view/liveBroadCast/list.vue
+++ src/view/liveBroadCast/list.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="wapper">
-    <div v-for="iteam in liveBroadCasts" :key="iteam.id" class="iteam">
-      <!-- <img src alt /> -->
-      <div class="iteam-banner" style="height:100%;">
-        <img :src="iteam.banner" />
-        <div class="watch">
-          <i class="iconfont icon-watch"></i>
-          {{iteam.hits}}
+    <scroll class="scroll" :data="liveBroadCasts" pulldown @pulldown="pulldown">
+      <div>
+        <div v-for="iteam in liveBroadCasts" :key="iteam.id" class="iteam">
+          <!-- <img src alt /> -->
+          <div class="iteam-banner" style="height:100%;">
+            <img :src="iteam.banner" />
+            <div class="watch">
+              <i class="iconfont icon-watch"></i>
+              {{iteam.hits}}
+            </div>
+          </div>
+          <p>{{iteam.title}}</p>
         </div>
       </div>
-      <p>{{iteam.title}}</p>
-    </div>
+    </scroll>
   </div>
 </template>
 
 <script>
 import { api_GetLiveBroadCast } from "@/api/api";
+import scroll from "@/base/scroll/scroll";
 export default {
   name: "liveBroadCast",
   props: [""],
@@ -25,7 +30,7 @@ export default {
     };
   },
 
-  components: {},
+  components: { scroll },
 
   computed: {},
 
@@ -41,35 +46,44 @@ export default {
     });
   },
 
-  methods: {},
+  methods: {
+    pulldown() {
+      console.log("aaaa");
+    }
+  },
 
   watch: {}
 };
 </script>
 <style lang='scss' scoped>
 .wapper {
-  .iteam {
-    margin-left: 10px;
-    margin-right: 10px;
-    margin-bottom: 20px;
-    .iteam-banner {
-      position: relative;
-      img {
-        width: 100%;
+  .scroll {
+    width: 100%;
+    height: 50vh;
+    overflow: hidden;
+    .iteam {
+      margin-left: 10px;
+      margin-right: 10px;
+      margin-bottom: 20px;
+      .iteam-banner {
+        position: relative;
+        img {
+          width: 100%;
+        }
+        .watch {
+          position: absolute;
+          bottom: 3px;
+          right: 0px;
+          color: white;
+          padding-left: 10px;
+          padding-right: 10px;
+          background-color: rgba(0, 0, 0, 0.3);
+        }
       }
-      .watch {
-        position: absolute;
-        bottom: 3px;
-        right: 0px;
-        color: white;
-        padding-left: 10px;
-        padding-right: 10px;
-        background-color: rgba(0, 0, 0, 0.3);
+      p {
+        margin-top: 5px;
+        color: #333;
       }
-    }
-    p {
-      margin-top: 5px;
-      color: #333;
     }
   }
 }
